a1bdcb1ae7840fb7bc5b9454766b515241f46f5e
hui0135 <hy010305@gmail.com>
2023-01-16 12:01:06 +0900

    Initial commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+# # from travel import *
+# # trip_to = vietnam.VietnamPackage()
+# # # trip_to.detail()
+
+from travel import *
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
+
+# import os
+# os.getcwd()
